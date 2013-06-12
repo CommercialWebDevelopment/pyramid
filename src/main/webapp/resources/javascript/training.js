c8eede4922e8e5750a9457ceec0e63593f2373b1
dbudunov <d.budunov@gmail.com>
2013-06-12 13:41:54 +0400

    small addition

--- src/main/webapp/resources/javascript/training.js
+++ src/main/webapp/resources/javascript/training.js
@@ -2,5 +2,5 @@ function load(videoId) {
     var url = 'http://www.youtube.com/embed/' + videoId + '?enablejsapi=1&origin=';
     var player = $("#player");
     player[0].src = url;
-    $(document).scrollTop(player[0].scrollTop);
+    $(document).scrollTop(player[0].scrollTop + 50);
 }
