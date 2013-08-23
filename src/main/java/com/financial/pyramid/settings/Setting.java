950e7b02a599165f5087e4c7c259a9183f484940
dbudunov <d.budunov@gmail.com>
2013-08-23 14:37:41 +0400

    Refactoring: added SettingProperty class (reformat code)

--- src/main/java/com/financial/pyramid/settings/Setting.java
+++ src/main/java/com/financial/pyramid/settings/Setting.java
@@ -7,23 +7,23 @@ package com.financial.pyramid.settings;
  */
 public class Setting {
 
-    public static String FEEDBACK_RECEIVER_EMAIL="feedbackReceiverEmail";
-    public static String OFFICE_PRICE="officePrice";
-    public static String MAX_ALLOWED_AMOUNT="maxAllowedAmount";
-    public static String APPLICATION_URL="applicationURL";
+    public static String FEEDBACK_RECEIVER_EMAIL = "feedbackReceiverEmail";
+    public static String OFFICE_PRICE = "officePrice";
+    public static String MAX_ALLOWED_AMOUNT = "maxAllowedAmount";
+    public static String APPLICATION_URL = "applicationURL";
 
-    public static String PAY_PAL_LOGIN="PAY_PAL_LOGIN";
+    public static String PAY_PAL_LOGIN = "PAY_PAL_LOGIN";
 
-    public static String YOU_TUBE_VIDEO_THUMBNAILS="youTubeVideoThumbnailsUrl";
-    public static String YOU_TUBE_VIDEO_URL="youTubeVideoUrl";
+    public static String YOU_TUBE_VIDEO_THUMBNAILS = "youTubeVideoThumbnailsUrl";
+    public static String YOU_TUBE_VIDEO_URL = "youTubeVideoUrl";
 
-    public static String CONTACTS_ADDRESS="contacts.address";
-    public static String CONTACTS_PHONES="contacts.phones";
-    public static String CONTACTS_MAP_URL="contacts.map";
-    public static String CONTACTS_MAP_HREF="contacts.showFullMap";
+    public static String CONTACTS_ADDRESS = "contacts.address";
+    public static String CONTACTS_PHONES = "contacts.phones";
+    public static String CONTACTS_MAP_URL = "contacts.map";
+    public static String CONTACTS_MAP_HREF = "contacts.showFullMap";
 
-    public static String PAY_PAL_API_USERNAME="pay_pal_api_username";
-    public static String PAY_PAL_API_PASSWORD="pay_pal_api_password";
-    public static String PAY_PAL_API_SIGNATURE="pay_pal_api_signature";
-    public static String PAY_PAL_API_APPLICATION_ID="pay_pal_application_id";
+    public static String PAY_PAL_API_USERNAME = "pay_pal_api_username";
+    public static String PAY_PAL_API_PASSWORD = "pay_pal_api_password";
+    public static String PAY_PAL_API_SIGNATURE = "pay_pal_api_signature";
+    public static String PAY_PAL_API_APPLICATION_ID = "pay_pal_application_id";
 }
